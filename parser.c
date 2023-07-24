9c02872c5a742b02b26a48b43e3f53ea89d7200e
onesimo5 <onesimojora57@gmail.com>
2023-07-24 14:55:04 -0700

    Created parser.c

---
+++ parser.c
@@ -0,0 +1,85 @@
+#include "shell.h"
+
+/**
+ * is_cmd - determines if file is executable cmd.
+ * @info: the info structure.
+ * @path: path to file.
+ * Return: 1 if true or otherwsie 0.
+ */
+int is_cmd(info_t *info, char *path)
+{
+	struct stat st;
+
+	(void)info;
+	if (!path || stat(path, &st))
+		return (0);
+
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * dup_chars - copies the characters.
+ * @pathstr: is the PATH string.
+ * @start: The starting index.
+ * @stop: The stopping index.
+ *
+ * Return: the pointer to the new buffer.
+ */
+char *dup_chars(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int i = 0, k = 0;
+
+	for (k = 0, i = start; i < stop; i++)
+		if (pathstr[i] != ':')
+			buf[k++] = pathstr[i];
+	buf[k] = 0;
+	return (buf);
+}
+
+/**
+ * find_path - finds cmd in the PATH str
+ * @info: the info structure
+ * @pathstr: PATH str.
+ * @cmd: command to find.
+ *
+ * Return: full path of commandd if found.
+ */
+char *find_path(info_t *info, char *pathstr, char *cmd)
+{
+	int i = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathstr)
+		return (NULL);
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	{
+		if (is_cmd(info, cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathstr[i] || pathstr[i] == ':')
+		{
+			path = dup_chars(pathstr, curr_pos, i);
+			if (!*path)
+				_strcat(path, cmd);
+			else
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			if (is_cmd(info, path))
+				return (path);
+			if (!pathstr[i])
+				break;
+			curr_pos = i;
+		}
+		i++;
+	}
+	return (NULL);
+}
