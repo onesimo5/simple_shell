63fb0354ce3ec87c82664163736a36ee11e2e262
onesimo5 <onesimojora57@gmail.com>
2023-07-26 06:50:09 -0700

    Newly Updated getLine.c file

--- getLine.c
+++ getLine.c
@@ -17,23 +17,19 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 		free(*buf);
 		*buf = NULL;
 		signal(SIGINT, sigintHandler);
-#if USE_GETLINE
 		r = getline(buf, &len_p, stdin);
-#else
 		r = _getline(info, buf, &len_p);
-#endif
+
 		if (r > 0)
 		{
 			if ((*buf)[r - 1] == '\n')
 			{
-<<<<<<< HEAD
 				(*buf)[r - 1] = '\0';
 				r--;
-=======
 				(*buf)[e - 1] = '\0';
 				e--;
->>>>>>> efe21a8a5b747ea9accdc0fdc5c9558b4caf45c0
 			}
+
 			info->linecount_flag = 1;
 			remove_comments(*buf);
 			build_history_list(info, *buf, info->histcount++);
