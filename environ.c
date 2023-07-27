83c59cc30401346e240a3b572223472b2d063248
onesimo5 <onesimojora57@gmail.com>
2023-07-27 16:09:25 -0700

    new environ.c

--- environ.c
+++ environ.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * _myenv - executes the current environ.
- * @info: struct containing arguments
- * used to maintain the function the prototype.
- * Return: Always 0 (Success).
+ * _myenv - prints the current environment
+ * @info: struct contain arguments. For maintaining
+ * const function prototype.
+ * Return: always 0.
  */
 int _myenv(info_t *info)
 {
@@ -13,37 +13,38 @@ int _myenv(info_t *info)
 }
 
 /**
- * _getenv - gets value of environ var.
- * @info: struct containing arguments
- * used to maintain the funtion of the prototype.
+ * _getenv - gets value of environ variable.
+ * @info: struct contains potent argumnets for maintaining
  * @name: environ var name.
+ *
  * Return: value.
  */
 char *_getenv(info_t *info, const char *name)
 {
 	list_t *node = info->env;
-	char *q;
+	char *p;
 
 	while (node)
 	{
-		q = starts_with(node->str, name);
-		if (q && *q)
-			return (q);
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
 		node = node->next;
 	}
 	return (NULL);
 }
 
 /**
- * _mysetenv - Inputs a new environ var or changes an existing one.
- * @info: struct containing arguments,used to maintain a function prototype.
- *  Return: 0.
+ * _mysetenv - init's a new env var,or modifies existing one.
+ * @info: struct contains potent arguments.used to maintain
+ * constant functn prototype.
+ * Return: always 0.
  */
 int _mysetenv(info_t *info)
 {
 	if (info->argc != 3)
 	{
-		_eputs("Incorrect number of arguements\n");
+		_eputs("Incorrect numb of arguements\n");
 		return (1);
 	}
 	if (_setenv(info, info->argv[1], info->argv[2]))
@@ -53,8 +54,9 @@ int _mysetenv(info_t *info)
 
 /**
  * _myunsetenv - deletes environ var.
- * @info: struct containing arguments,used to maintain function prototype.
- *  Return: Always 0 (Success).
+ * @info: struct contains potent arguments,use maintain
+ * const function prototype.
+ * Return: always 0.
  */
 int _myunsetenv(info_t *info)
 {
@@ -73,17 +75,17 @@ int _myunsetenv(info_t *info)
 
 /**
  * populate_env_list - populates the env linked list.
- * @info: struct containing arguments
- * used to maintain a constant function prototype.
- * Return: Always 0 (Success).
+ * @info: struct contains potent arguments,for maintaining
+ * function prototype.
+ * Return: always 0.
  */
 int populate_env_list(info_t *info)
 {
 	list_t *node = NULL;
-	size_t i;
+	size_t a;
 
-	for (i = 0; environ[i]; i++)
-		add_node_end(&node, environ[i], 0);
+	for (a = 0; environ[a]; a++)
+		add_node_end(&node, environ[a], 0);
 	info->env = node;
 	return (0);
 }
